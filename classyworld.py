274356ba999603c45b2358a8d3071a216b8acd71
harpreet14.singh <harpreet14.singh@paytm.com>
2022-02-24 18:08:34 -0500

    initial linkedlist

---
+++ classyworld.py
@@ -0,0 +1,74 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+
+my_linked_list = LinkedList(11)
+my_linked_list.append(3)
+my_linked_list.append(23)
+my_linked_list.append(7)
+
+my_linked_list.print_list()
+
+class Cookie:
+    def __init__(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
+
+    def set_color(self, color):
+        self.color = color
+
+
+cookie_one = Cookie('green')
+cookie_two = Cookie('blue')
+
+print('Cookie one is', cookie_one.get_color())
+print('Cookie two is', cookie_two.get_color())
+
+cookie_one.set_color('yellow')
+
+print('\nCookie one is now', cookie_one.get_color())
+print('Cookie two is still', cookie_two.get_color())
+
+
+
+# dictionaries are using same location in memoory
+dict1 = { 'value':11}
+dict2 = dict1
+
+print(dict1)
+print(dict2)
+
+dict1['value'] = 12
+print(dict1)
+print(dict2)
+
+# List is continous in memory ---- Linked List is spread around and dont have indexes.
