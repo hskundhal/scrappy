5c6b932e7a2dd279d43dd9d120c8118aa7ae270a
Harpreet Singh <hskundhal@gmail.com>
2022-03-06 18:44:32 -0500

    dutchflagproblem

---
+++ dutchflagsorting.py
@@ -0,0 +1,20 @@
+def dutchflagsort(array):
+    i = 0
+    j = 0
+    k = len(array)-1
+    mid = 1
+    while (j < k):
+
+        if (array[j]<mid):
+            array[i],array[j] = array[j],array[i]
+            i+=1
+            j+=1
+        elif array[j]==mid:
+            
+            j+=1
+        elif array[j]>mid:
+            array[j],array[k]=array[k],array[j]
+            k-=1
+            
+    print(array)
+dutchflagsort([1,1,2,0,1,0])
